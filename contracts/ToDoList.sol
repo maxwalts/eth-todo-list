dbf3a7e459014b30ea9c0ee4c69097151462291e
mwalts <mwalts@umich.edu>
2020-08-13 17:34:22 -0500

    adding src/ for frontend. intex.html from source material with added notes.

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -5,4 +5,22 @@ pragma solidity ^0.5.0;
  */
 contract ToDoList {
   uint public taskCount = 0;
+
+  struct Task {
+  	uint id;
+  	string content;
+  	bool isCompleted;
+  }
+
+  mapping (uint => Task) public tasks;
+
+  constructor() public {
+  	createTask("This is a great first sample task.");
+  }
+
+  function createTask(string memory _content) public {
+  	taskCount++;
+  	tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 } 
